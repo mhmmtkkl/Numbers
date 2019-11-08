4cfc0ced9fa8b1dbd2af99648c00dd2287879596
redflame <dosmukhamed@gmail.com>
2019-11-08 07:28:41 -0500

    refactoring code

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,190 +1,129 @@
-import org.junit.Test;
-
 import java.util.ArrayList;
-import java.util.List;
 
 public class Numbers {
 
-
-//    split
-
-//    tocharArray
-
-//    char , arraylist -->>  for loop la birlikte toplami bulma
-
-//    even rakamlarin toplamini bulma
-
-//    odd rakamlarin toplammini bulma
-
-//
-
-    ArrayList<Integer> Numbers;
-
-    String MyString;
-
-    public Numbers() {
-
-
-    }
-
-
-
-    /*
-            You have string number get the each characters from the string and return the char array
-
-            hint use tochararray
+    /**
+     * 1. split
+     * 2. toCharArray
+     * 3. char, ArrayList, calculate sum using for loop
+     * 4. calculate even numbers sum
+     * 5. calculate odd numbers sum
+     * 6. upper case
+     * 7. lower case
      */
-    public char[] test1(String number) {
 
-        char[] eachWord;
-
-        //        write your code here ----->>>>
+    /**
+     * You have string "number", get each character from the string and return the char array
+     * Hint: use toCharArray method
+     */
+    public char[] getChars(String number) {
+        char[] eachWord = {};
+        // write your code here
 
         eachWord = number.toCharArray();
 
+        // end of code
         return eachWord;
-
     }
 
-    /*
-        You have string "sentence" get the each word from the string and return string array
-
-        hint use split
- */
-    public String[] test2(String sentence) {
-
-        String[] eachWord;
-
-        //        write your code here ----->>>>
+    /**
+     * You have string "sentence", get each word from the string and return string array
+     * Hint: use split method
+     */
+    public String[] getWords(String sentence) {
+        String[] eachWord = {};
+        // write your code here
 
         eachWord = sentence.split(" ");
 
+        // end of code
         return eachWord;
     }
 
-/*
-        You have an arrayList name is numbers
-
-        numbers arraylist have numbers like >> 1,2,3,4
-
-        calculate the total numbers 1+2+3+4 = 10
-
- */
-
-    public int test3(ArrayList<Integer> myList) {
-
+    /**
+     * Calculate sum of numbers
+     * ex: arrayList is: 1, 2 ,3 ,4
+     * 1+2+3+4 = 10
+     * total should be 10;
+     */
+    public int total(ArrayList<Integer> myList) {
         int total = 0;
-
-//        Write your code here  --->>
-
+        // write your code here
 
         for (int i = 0; i < myList.size(); i++) {
-
             total = total + myList.get(i);
-
         }
 
-//       <<----
-
+        // end of code
         return total;
-
     }
 
-
-    /*
-           get the total numbers of the even numbers
-
-           for example if arrayList has the 1,2,3,4,5,6,7,8,9
-
-           2+4+6+8 = 20
-
-            total should be 20 ;
-
-    */
-    public int test4(ArrayList<Integer> MyNumbers) {
-
-        int total = 0;
-
-        //        write your code here ----->>>>
-
-        for (int i = 0; i < MyNumbers.size(); i++) {
-
-            if (MyNumbers.get(i) % 2 == 0) {
-
-                total = total + MyNumbers.get(i);
-
-            }
-
-        }
-
-        return total;
-
-    }
-
-    /*
-           get the total numbers of the odd numbers
-
-           for example if arrayList has the 1,2,3,4,5,6,7,8,9
-
-           1+3+5+7+9 = 25
-
-            total should be 25 ;
-
-    */
-    public int test5(ArrayList<Integer> MyNumbers) {
-
-        int total = 0;
-
-        //        write your code here ----->>>>
-
-        for (int i = 0; i < MyNumbers.size(); i++) {
-
-            if (MyNumbers.get(i) % 2 == 1) {
-
-                total = total + MyNumbers.get(i);
-
-            }
-
-        }
-
-
-        return total;
-
-    }
-
-
-    /*
-        make the name upper case
-
-      */
-    @Test
-    public String test6(String Name) {
-        String UpperCaseName;
-
-        //        write your code here ----->>>>
-
-        UpperCaseName = Name.toUpperCase();
-
-        return UpperCaseName;
-
-    }
-
-    /*
-       make the name Lower case
-
+    /**
+     * Calculate sum of even numbers
+     * <p>
+     * ex: arrayList is: 1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9
+     * 2+4+6+8 = 20
+     * total should be 20;
      */
-    @Test
-    public String test7(String Name) {
-        String LowerCaseName;
+    public int evenTotal(ArrayList<Integer> MyNumbers) {
+        int total = 0;
+        // write your code here
 
-//        write your code here ----->>>>
+        for (int i = 0; i < MyNumbers.size(); i++) {
+            if (MyNumbers.get(i) % 2 == 0) {
+                total = total + MyNumbers.get(i);
+            }
+        }
 
-        LowerCaseName = Name.toLowerCase();
+        // end of code
+        return total;
+    }
 
+    /**
+     * Calculate sum of odd numbers
+     * <p>
+     * ex: arrayList is: 1, 2 ,3 ,4 ,5 ,6 ,7 ,8 ,9
+     * 1+3+5+7+9 = 25
+     * total should be 25;
+     */
+    public int oddTotal(ArrayList<Integer> MyNumbers) {
+        int total = 0;
+        // write your code here
 
-        return LowerCaseName;
+        for (int i = 0; i < MyNumbers.size(); i++) {
+            if (MyNumbers.get(i) % 2 == 1) {
+                total = total + MyNumbers.get(i);
+            }
+        }
 
+        // end of code
+        return total;
     }
 
 
+    /**
+     * Make the name upper case
+     */
+    public String getUpperCase(String name) {
+        String upperCaseName = null;
+        // write your code here
+
+        upperCaseName = name.toUpperCase();
+
+        // end of code
+        return upperCaseName;
+    }
+
+    /**
+     * Make the name lower case
+     */
+    public String getLowerCase(String name) {
+        String lowerCaseName = "";
+        // write your code here
+
+        lowerCaseName = name.toLowerCase();
+
+        // end of code
+        return lowerCaseName;
+    }
 }
